41eccba750e86fb199ff5e30b5a1c3949dcb09b9
cmonsalt <cristiantm12.com>
2022-03-22 20:29:59 -0500

    Styled Components & Single File Components

--- app/components/Router.js
+++ app/components/Router.js
@@ -1,5 +1,6 @@
 import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js";
+import { ContactForm } from "./ContactForm.js";
 import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
 import { SearchCard } from "./SearchCard.js";
@@ -47,7 +48,7 @@ export async function Router() {
       },
     });
   } else if (hash === "#/contacto") {
-    document.getElementById("main").innerHTML = `<h2>Sección de Contacto</h2>`;
+    document.getElementById("main").appendChild(ContactForm());
   } else {
     await ajax({
       url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
